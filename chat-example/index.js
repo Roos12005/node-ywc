3351d7f7f05be10deb02e3bf7c3ac7844998f0dc
Roos <Vasvaroos@gmail.com>
2016-03-14 02:20:15 +0700

    final

--- chat-example/index.js
+++ chat-example/index.js
@@ -11,6 +11,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', (socket) => {
+	console.log('Found some user connected')
+
+	// Server get a message
+	socket.on('chat message', function (msg) {
+		console.log(msg)
+		// Server broadcast a message
+		// socket.emit('update message', msg)
+		io.emit('update message', msg)
+	})
+
+
+})
+
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`)
 })
